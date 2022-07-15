dbe164e69c8c314f6ac4a7133832ea81b79b60f9
K, Prakruthi <prakrk@deloitte.com>
2022-07-15 17:42:16 +0530

    Restructured the folder and files

--- Config/configParser.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-
-def getConfig(key):
-    parser = configparser.ConfigParser()
-    parser.read("C://Users//prakrk//PycharmProjects//EmbeddedTestingMockAPI//Config//config.ini")
-    result = parser['API'][key]
-    return result
